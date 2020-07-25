e52c8175779a5882efc9851932172015c7ca483b
sudbala <sudharsan.bala@yahoo.com>
2020-07-25 18:51:30 -0400

    Search Bar App Bar (In Progress)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:on_night/screens/OnNight.dart';
 
 void main() {
+  //SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(OnNight());
 }
--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flappy_search_bar/flappy_search_bar.dart';
+import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -15,31 +17,30 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-Map<int, Color> darkCorn =
-{
-  50:Color.fromRGBO(18, 19, 31, .1),
-  100:Color.fromRGBO(18, 19, 31,.2),
-  200:Color.fromRGBO(18, 19, 31, .3),
-  300:Color.fromRGBO(18, 19, 31, .4),
-  400:Color.fromRGBO(18, 19, 31,.5),
-  500:Color.fromRGBO(18, 19, 31, .6),
-  600:Color.fromRGBO(18, 19, 31, .7),
-  700:Color.fromRGBO(18, 19, 31, .8),
-  800:Color.fromRGBO(18, 19, 31, .9),
-  900:Color.fromRGBO(18, 19, 31, 1),
+Map<int, Color> darkCorn = {
+  50: Color.fromRGBO(18, 19, 31, .1),
+  100: Color.fromRGBO(18, 19, 31, .2),
+  200: Color.fromRGBO(18, 19, 31, .3),
+  300: Color.fromRGBO(18, 19, 31, .4),
+  400: Color.fromRGBO(18, 19, 31, .5),
+  500: Color.fromRGBO(18, 19, 31, .6),
+  600: Color.fromRGBO(18, 19, 31, .7),
+  700: Color.fromRGBO(18, 19, 31, .8),
+  800: Color.fromRGBO(18, 19, 31, .9),
+  900: Color.fromRGBO(18, 19, 31, 1),
 };
 
-MaterialColor darkCornColor = MaterialColor(0xFF12131F , darkCorn);
+MaterialColor darkCornColor = MaterialColor(0xFF12131F, darkCorn);
 
 class _HomePageState extends State<HomePage> {
   // WebView Controller
   final Completer<WebViewController> _controller =
-  Completer<WebViewController>();
+      Completer<WebViewController>();
 
   // Current selected tab
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Index 0: Frackets',
@@ -72,72 +73,72 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-          alignment: Alignment.bottomCenter,
-          children: <Widget>[
-            WebView(
-              initialUrl: 'https://onnight-1403b.web.app/',
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController controller) {
-                _controller.complete(controller);
-              },
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                height: 70,
-                child: AppBar(
-                  // Here we take the value from the MyHomePage object that was created by
-                  // the App.build method, and use it to set our appbar title.
-                  title: Text(widget.title),
-                  actions: <Widget>[
-                    IconButton(
-                    icon: Icon(Icons.settings),
-                    color: Colors.white70)],
-                  backgroundColor: Colors.transparent,
-                ),
+      body: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+        WebView(
+          initialUrl: 'https://onnight-1403b.web.app/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        Align(
+          alignment: FractionalOffset(0.5, 0.05),
+          child: Container(
+            height: 150,
+            child: SizedBox(
+              height: 10,
+              width: 250,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 150,
+                    child: SearchBar(
+                      iconActiveColor: Colors.white,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      searchBarStyle: SearchBarStyle(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: CurvedNavigationBar(
+//            AppBar(
+//              // Here we take the value from the MyHomePage object that was created by
+//              // the App.build method, and use it to set our appbar title.
+//
+//              actions: <Widget>[
+//                IconButton(icon: Icon(Icons.settings), color: Colors.white70),
+//              ],
+//              backgroundColor: Colors.transparent,
+//            ),
+          ),
+        ),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: CurvedNavigationBar(
               height: 50,
               backgroundColor: Colors.transparent,
               buttonBackgroundColor: Colors.purple,
-
               color: darkCornColor,
               items: <Widget>[
                 Container(
-                  child: SvgPicture.asset(
-                    'assets/fracket.svg',
-                    width: 40,
-                    height: 40,
-                    color: Colors.white70
-                  ),
+                  child: SvgPicture.asset('assets/fracket.svg',
+                      width: 40, height: 40, color: Colors.white70),
                 ),
                 Container(
-                      child: SvgPicture.asset(
-                        'assets/DartmouthSocial.svg',
-                        width: 40,
-                        height: 40,
-                        color: Colors.white70
-                      ),
-                    ),
-                Container(
-                  child: SvgPicture.asset(
-                    'assets/Map.svg',
-                    width: 40,
-                    height: 40,
-                    color: Colors.white70
-                  ),
+                  child: SvgPicture.asset('assets/DartmouthSocial.svg',
+                      width: 40, height: 40, color: Colors.white70),
                 ),
                 Container(
-                  child: SvgPicture.asset(
-                    'assets/Calendar.svg',
-                    width: 40,
-                    height: 40,
-                    color: Colors.white70
-                  ),
+                  child: SvgPicture.asset('assets/Map.svg',
+                      width: 40, height: 40, color: Colors.white70),
+                ),
+                Container(
+                  child: SvgPicture.asset('assets/Calendar.svg',
+                      width: 40, height: 40, color: Colors.white70),
                 ),
                 Container(
                   child: SvgPicture.asset(
@@ -152,8 +153,7 @@ class _HomePageState extends State<HomePage> {
                 //Handle button tap
               },
             ))
-          ]
-      ),
+      ]),
     );
   }
 }
