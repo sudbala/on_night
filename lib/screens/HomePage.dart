9482d73ac7575c8be3c4de9074e630726b3f9ae8
Maria Roodnitsky <maria.roodnitsky.22@dartmouth.edu>
2020-07-24 21:21:41 -0500

    maria loves sud <3

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -13,15 +15,31 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+Map<int, Color> darkCorn =
+{
+  50:Color.fromRGBO(18, 19, 31, .1),
+  100:Color.fromRGBO(18, 19, 31,.2),
+  200:Color.fromRGBO(18, 19, 31, .3),
+  300:Color.fromRGBO(18, 19, 31, .4),
+  400:Color.fromRGBO(18, 19, 31,.5),
+  500:Color.fromRGBO(18, 19, 31, .6),
+  600:Color.fromRGBO(18, 19, 31, .7),
+  700:Color.fromRGBO(18, 19, 31, .8),
+  800:Color.fromRGBO(18, 19, 31, .9),
+  900:Color.fromRGBO(18, 19, 31, 1),
+};
+
+MaterialColor darkCornColor = MaterialColor(0xFF12131F , darkCorn);
+
 class _HomePageState extends State<HomePage> {
   // WebView Controller
   final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
+  Completer<WebViewController>();
 
   // Current selected tab
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Index 0: Frackets',
@@ -58,70 +76,31 @@ class _HomePageState extends State<HomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: darkCornColor,
       ),
-      body: WebView(
-        initialUrl: 'https://onnight-1403b.web.app/',
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Container(
-              child: SvgPicture.asset(
-                'assets/fracket.svg',
-                width: 24,
-                height: 24,
-              ),
+      body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            WebView(
+              initialUrl: 'https://onnight-1403b.web.app/',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController controller) {
+                _controller.complete(controller);
+              },
             ),
-            title: Text('Frackets'),
-          ),
-          BottomNavigationBarItem(
-              icon: Container(
-                child: SvgPicture.asset(
-                  'assets/DartmouthSocial.svg',
-                  width: 24,
-                  height: 24,
-                ),
-              ),
-              title: Text('Social')),
-          BottomNavigationBarItem(
-            icon: Container(
-              child: SvgPicture.asset(
-                'assets/Map.svg',
-                width: 24,
-                height: 24,
-              ),
-            ),
-            title: Text('Map'),
-          ),
-          BottomNavigationBarItem(
-            icon: Container(
-              child: SvgPicture.asset(
-                'assets/Calendar.svg',
-                width: 24,
-                height: 24,
-              ),
-            ),
-            title: Text('Calendar'),
-          ),
-          BottomNavigationBarItem(
-            icon: Container(
-              child: SvgPicture.asset(
-                'assets/profile.svg',
-                width: 24,
-                height: 24,
-              ),
-            ),
-            title: Text('Profile'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+            CurvedNavigationBar(
+              height: 50,
+              backgroundColor: Colors.transparent,
+              items: <Widget>[
+                Icon(Icons.add, size: 30),
+                Icon(Icons.list, size: 30),
+                Icon(Icons.compare_arrows, size: 30),
+              ],
+              onTap: (index) {
+                //Handle button tap
+              },
+            )
+          ]
       ),
     );
   }
