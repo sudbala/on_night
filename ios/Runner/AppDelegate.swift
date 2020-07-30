c503e8292a4a3b4bc7677a545146a78bc2f439a3
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2020-07-30 03:56:10 -0500

    a great commit message was wiped out; map mostly done womp

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+        GMSServices.provideAPIKey("AIzaSyB_xyTfgcGBiiel6xZcI8at908ZI6bb3qE")
+
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/screens/BlankPage.dart
+++ lib/screens/BlankPage.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 /// This BlankPage Widget is a completely Blank Widget with a custom listView
 /// added to it in order to serve as our 'placeholder' screens for now
@@ -10,18 +14,42 @@ class BlankPage extends StatefulWidget {
 }
 
 class _BlankPageState extends State<BlankPage> {
+
+  GoogleMapController _mapController;
+  bool _showMapStyle = false;
+
+  void _onMapCreated(GoogleMapController controller){
+    _mapController = controller;
+
+    setState(() {
+
+    });
+
+    _setMapStyle();
+  }
+
+  void _setMapStyle() async {
+    String style = await DefaultAssetBundle.of(context).loadString('assets/map_style.json');
+    print('mrp yes this is happening');
+    _mapController.setMapStyle(style);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("First Screen"),
+        title: Text("Maria's Screen"),
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Lorem Ipsum'),
-          subtitle: Text('$index'),
-        );
-      }),
+      body: GoogleMap(
+
+        onMapCreated: _onMapCreated,
+          buildingsEnabled: false,
+          zoomControlsEnabled: false,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(43.704871, -72.288735), zoom: 17, tilt: 6,
+              )),
+
     );
   }
 }
