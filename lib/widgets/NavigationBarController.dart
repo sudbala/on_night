a05d7a559f4cb86c452349fd205e78e997027ad8
sudbala <sudharsan.bala@yahoo.com>
2020-12-04 14:40:13 -0500

    files in good places now

--- lib/widgets/NavigationBarController.dart
+++ lib/widgets/NavigationBarController.dart
@@ -1,4 +1,4 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:on_night/screens/Map.dart';
